bda0777b26481618255d25a542dcbf00ab7a9b1e
Lindsay Evans <linz@linz.id.au>
2011-08-20 10:58:21 +1000

    Adding 'more soon' msg; fixin default no-format bug; adding random names

--- app.rb
+++ app.rb
@@ -35,18 +35,43 @@ get '/:dataset.:format' do
 end
 
 get '/:dataset' do
-	render_dataset(params[:dataset], 'html')
+	render_dataset(get_dataset(params[:dataset]), 'html')
 end
 
 def get_dataset dataset_name
-	# TODO: load these from disk or something instead, anonymise
+	# TODO: load these from disk or something instead
 	if dataset_name == 'employees'
 		return [
-			{ :id => 1, :first_name => 'Lindsay', :last_name => 'Evans'},
-			{ :id => 2, :first_name => 'Fred', :last_name => 'Randell'},
-			{ :id => 3, :first_name => 'Grant', :last_name => 'Klein'},
-			{ :id => 4, :first_name => 'Danny', :last_name => 'Webster'},
-			{ :id => 5, :first_name => 'Sharyn', :last_name => 'Persijn'}
+			{ :id => 1, :first_name => 'Clarence', :last_name => 'Binford'},
+			{ :id => 2, :first_name => 'Janet', :last_name => 'Chapa'},
+			{ :id => 3, :first_name => 'Henry', :last_name => 'Nieto'},
+			{ :id => 4, :first_name => 'Myrtle', :last_name => 'Shell'},
+			{ :id => 5, :first_name => 'Georgia', :last_name => 'Maggio'},
+			{ :id => 6, :first_name => 'Jeremy', :last_name => 'Duque'},
+			{ :id => 7, :first_name => 'Margie', :last_name => 'Wheaton'},
+			{ :id => 8, :first_name => 'Dale', :last_name => 'Bloch'},
+			{ :id => 9, :first_name => 'Larry', :last_name => 'Ringer'},
+			{ :id => 10, :first_name => 'Jose', :last_name => 'Sanabria'},
+			{ :id => 11, :first_name => 'Adam', :last_name => 'Page'},
+			{ :id => 12, :first_name => 'Juan', :last_name => 'Backus'},
+			{ :id => 13, :first_name => 'Natalie', :last_name => 'Shirley'},
+			{ :id => 14, :first_name => 'Albert', :last_name => 'Beaver'},
+			{ :id => 15, :first_name => 'George', :last_name => 'Horton'},
+			{ :id => 16, :first_name => 'Katrina', :last_name => 'Leathers'},
+			{ :id => 17, :first_name => 'Russell', :last_name => 'Adorno'},
+			{ :id => 18, :first_name => 'Bernice', :last_name => 'Braithwaite'},
+			{ :id => 19, :first_name => 'Janie', :last_name => 'Thrasher'},
+			{ :id => 20, :first_name => 'Adam', :last_name => 'Troupe'},
+			{ :id => 21, :first_name => 'Celia', :last_name => 'Nowlin'},
+			{ :id => 22, :first_name => 'Stephanie', :last_name => 'Milne'},
+			{ :id => 23, :first_name => 'Nicholas', :last_name => 'Haire'},
+			{ :id => 24, :first_name => 'Clara', :last_name => 'Barham'},
+			{ :id => 25, :first_name => 'Clarence', :last_name => 'O\'Malley'},
+			{ :id => 26, :first_name => 'Lawrence', :last_name => 'Simone'},
+			{ :id => 27, :first_name => 'Chris', :last_name => 'Ouellette'},
+			{ :id => 28, :first_name => 'Pam', :last_name => 'Soukup'},
+			{ :id => 29, :first_name => 'Robert', :last_name => 'MacIntyre'},
+			{ :id => 30, :first_name => 'Gregory', :last_name => 'Pauley'}
 		]
 	end
 
